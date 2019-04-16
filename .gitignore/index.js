7477f5e8d672257880a634ac39168d8637a10b6c
LuckyKous <49690172+LuckyKous@users.noreply.github.com>
2019-04-16 18:40:45 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 var bot = new Discord.Client();
 
 bot.on("ready", function() {
-    bot.user.setGame("Le couscous c'est bon !");
+    bot.user.setGame("Le couscous, c'est bon !");
     console.log("Le bot a bien et connecte");
 });
 
