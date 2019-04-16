403b63dd31c35e6cc99edc5afd8e5f835ea48329
LuckyKous <aecikus@gmail.com>
2019-04-16 22:08:08 +0200

    F

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,32 +1,14 @@
 const Discord = require("discord.js");
+const client = new Discord.Client();
 
-var PREFIX = "!";
-
-const bot = new Discord.Client();
-
-bot.on("ready", function() {
-    bot.user.setGame("Le couscous, c'est bon !");
-    console.log("Le bot a bien et connecte");
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
-    if (msg.content === 'ping') {
-      msg.reply('Pong!');
-    }
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
 });
 
-bot.on("message", async function(message) {
-    if (message.auhor.equals(bot.user)) return;
-
-    if(!message.content.startsWith(PREFIX)) return;
-
-    var args = message.content.substring(PREFIX.length).split(" ");
-
-    switch(args[0].toLowerCase()) {
-        case "invite":
-        message.channel.send("Test");
-        break;
-    }
-});
-
-bot.login(process.env.TOKEN);
+client.login(process.env.TOKEN);
