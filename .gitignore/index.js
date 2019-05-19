5ccf2b19230881106ab430f9cbb5f41d17cb4282
LuckyKous <aecikus@gmail.com>
2019-05-19 22:14:42 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -2,6 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 client.on('ready', () => {
+  bot.user.setGame("Les carottes c'est bon pour la santé !");
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
